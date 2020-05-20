4ecf7217650691898690af738cde74b37b1eae81
V-Kunal Verma <Kunal.Verma@blackbaud.me>
2020-05-20 22:00:10 +0530

    track2

---
+++ precache-manifest.bd5eb2759810cf0ff302fe990846d3db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3543bf2370e1a847acb5cb451572c1",
+    "url": "/pocstore/index.html"
+  },
+  {
+    "revision": "1c8c85a35810594364bb",
+    "url": "/pocstore/static/css/main.eda9d507.chunk.css"
+  },
+  {
+    "revision": "5aca542f98850b17812e",
+    "url": "/pocstore/static/js/2.ff889646.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/pocstore/static/js/2.ff889646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8c85a35810594364bb",
+    "url": "/pocstore/static/js/main.81b71f7c.chunk.js"
+  },
+  {
+    "revision": "09512f0141c13bd56e05",
+    "url": "/pocstore/static/js/runtime-main.6ee5da82.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0aea7d8291d7d1b35ef6ff9d073cbe69.js"
+  "/pocstore/precache-manifest.bd5eb2759810cf0ff302fe990846d3db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pocstore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
